44c493f3f16f193c261244a11b2449063175e5ba
yingming25 <karcnonf@gmail.com>
2019-02-26 10:28:48 +0800

    add event listener for decimal

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('.decimal');
 const result = document.querySelector('.result');
+
 let data = [];
 let str = ``;
 let expression = [];
@@ -8,8 +10,14 @@ for(let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener('click', inputNumber, false);
 }
 
+decimal.addEventListener('click', inputNumber, false);
+
 function inputNumber(e) {
   const newNumber = e.target.innerHTML;
+
+  if(str.length === 0) {
+    str += "0";
+  }
 
   str += `${newNumber}`;
   data.push(Number(str));

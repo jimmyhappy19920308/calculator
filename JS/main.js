31486995532698e8a3f0b5ff6645a635dcd6f4e7
yingming25 <karcnonf@gmail.com>
2019-02-26 23:02:44 +0800

    add event listener for deciaml button
    change handler function's name

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,15 @@
 const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('.decimal');
 const result = document.querySelector('.result');
 let tempNumber = '0';
 
 for(let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', inputNumber, false);
+  numbers[i].addEventListener('click', inputValue, false);
 }
 
-function inputNumber(e) {
+decimal.addEventListener('click', inputValue, false);
+
+function inputValue(e) {
   tempNumber += e.target.innerHTML;
-  result.innerHTML = parseInt(tempNumber, 10);
+  result.innerHTML = tempNumber.slice(1);
 }
-
-

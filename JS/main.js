6cb9c1b4b5a273197fe2c044b69c6f46dcbd5d8c
yingming25 <karcnonf@gmail.com>
2019-02-26 12:04:35 +0800

    add event listener for number button
    get number button's value to display on result

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+const numbers = document.querySelectorAll('.number');
+const result = document.querySelector('.result');
+let tempNumber = '0';
+
+for(let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', inputNumber, false);
+}
+
+function inputNumber(e) {
+  console.log(e.target.innerHTML);
+  tempNumber += e.target.innerHTML;
+  result.innerHTML = parseInt(tempNumber, 10);
+}

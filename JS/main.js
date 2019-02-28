bc4b1820f87322caf2213a661f4621f047b0b80f
yingming25 <karcnonf@gmail.com>
2019-02-28 11:49:16 +0800

    resolve eslint error

--- JS/main.js
+++ JS/main.js
@@ -3,12 +3,6 @@ const decimal = document.querySelector('.decimal');
 const result = document.querySelector('.result');
 let tempNumber = '0';
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', inputValue, false);
-}
-
-decimal.addEventListener('click', inputValue, false);
-
 function inputValue(e) {
   const value = e.target.innerHTML;
 
@@ -17,9 +11,15 @@ function inputValue(e) {
   if (tempNumber === '0') {
     result.innerHTML = '0';
   } else if (tempNumber[0] === '0' && tempNumber[1] !== '.' && tempNumber.length >= 1) {
-    tempNumber = tempNumber[1];
+    [tempNumber] = [tempNumber[1]];
     result.innerHTML = tempNumber;
   } else {
     result.innerHTML = tempNumber;
   }
 }
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', inputValue, false);
+}
+
+decimal.addEventListener('click', inputValue, false);
